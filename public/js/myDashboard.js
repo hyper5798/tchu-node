e63682edd37fcf6bdbb88a023509146b5e743c16
hyper5798 <hyper5798@gmail.com>
2022-10-16 21:01:34 +0800

    Remark mac Voltage log

--- public/js/myDashboard.js
+++ public/js/myDashboard.js
@@ -23,7 +23,7 @@ for(let i=0; i<keys.length;i++) {
     macVoltageObject[mKey] = {"image": "/icons/battery/b1.png", "value": '5V'};
   }
 }
-console.log(JSON.stringify(macVoltageObject));
+//console.log(JSON.stringify(macVoltageObject));
 
 function updateGaugeValue(id, value) {
     //$(id)[0].contentWindow.changeValue(value);
