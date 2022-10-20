5f79f7326e03ad0d9ede6beee693b3a8db46748a
hyper5798 <hyper5798@gmail.com>
2022-10-20 21:12:50 +0800

    [0.1.31][Dashboard] Change query start date.

--- public/js/myDashboard.js
+++ public/js/myDashboard.js
@@ -220,7 +220,7 @@ function toQuery(mac){
   //$.LoadingOverlay("show");
   var now = new Date();
   var to = (now.getFullYear() + '/' + (now.getMonth() + 1) + '/' + now.getDate() );
-	var fromMoment = moment(to,"YYYY/MM/DD").subtract(31,'days');;
+	var fromMoment = moment(to,"YYYY/MM/DD").subtract(1,'days');;
 	var from =  fromMoment.format("YYYY/MM/DD");
   var url = host_url+'/todos/query?mac='+mac+'&from='+from+'&to='+to;
   url = url + '&queryType=queryEvent&userName=' + userName;
