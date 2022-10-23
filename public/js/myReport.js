aa111afb14a73181c8e39fd200b52e6bbbfb4b98
hyper5798 <hyper5798@gmail.com>
2022-10-23 12:17:23 +0800

    [0.1.34][Report] Modify for keep start date.

--- public/js/myReport.js
+++ public/js/myReport.js
@@ -209,7 +209,9 @@ function toQuery(){
   $.LoadingOverlay("show");
   $('#myModal').modal('hide');
   var from = $('#from').val();
+  app.startDate = from;
   var to = $('#to').val();
+  app.endDate = to;
   var url = host_url+'/todos/query?mac='+mac+'&from='+from+'&to='+to;
   url = url + '&queryType=queryEvent&userName=' + userName;
   console.log(url);
